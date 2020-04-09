5dd6811e73d0e40b39e85ae3e27aefdd2e303ade
xiaoc <1059029034@qq.com>
2020-04-09 09:39:11 +0800

    4-9

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,34 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import amapInit from '@/lib/amapInit.js'
+import Vue from "vue";
+import amapInit from "./lib/amapInit.js";
 export default Vue.extend({
-	created(){
-		amapInit.init()
+	created() {
+		amapInit.init();
+
+		// 在页面加载时读取sessionStorage里的状态信息
+		if (sessionStorage.getItem("store")) {
+			this.$store.replaceState(
+				Object.assign(
+					{},
+					this.$store.state,
+					JSON.parse(sessionStorage.getItem("store") as string)
+				)
+			);
+			// 数据读取设置完成后 清除sessionStorage数据
+			sessionStorage.removeItem("store");
+		}
+		// 在页面刷新时将vuex里的信息保存到sessionStorage里
+		window.addEventListener("beforeunload", () => {
+			sessionStorage.updateWeb = true;
+			sessionStorage.setItem(
+				"store",
+				JSON.stringify(this.$store.state) as string
+			);
+		});
 	}
-})
+});
 </script>
 
 <style lang="scss">
